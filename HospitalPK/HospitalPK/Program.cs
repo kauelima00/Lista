74fe440ad3faa8aa4af4bbefe6a059fd65965c2d
kauelima00 <klimaml022@gmail.com>
2026-06-07 13:52:33 -0300

    Add files via upload

---
+++ HospitalPK/HospitalPK/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalPK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string val = "w";
+            SistemaFila fila = new SistemaFila();
+
+            while (val != "q")
+            {
+                Console.WriteLine("---------->Menu<----------");
+
+                Console.WriteLine("[1] Cadastrar");
+                Console.WriteLine("[2] Lista dos Pacientes");
+                Console.WriteLine("[3] Atender Paciente ");
+                Console.WriteLine("[4] Alterar Cadastro");
+                Console.WriteLine("[q] Sair");
+
+                val = Console.ReadLine();
+
+                switch (val)
+                {
+                    case "1":
+                        fila.Cadastro();
+                        break;
+                    case "2":
+                        fila.Listar();
+                        break;
+                    case "3":
+                        fila.Atender();
+                        break;
+                    case "4":
+                        fila.Alterar();
+                        break;
+                    case "q":
+                    case "Q":
+                        Console.WriteLine("\nSaindo do program...");
+                        break;
+                    default:
+                        Console.WriteLine("\nDigite numero valido do Menu!");
+                        break;
+
+                }
+            }
+        }
+    }
+}
+
+        
+   
+
